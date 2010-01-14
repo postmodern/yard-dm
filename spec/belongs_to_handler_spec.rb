089a398a9630ad0f259d0fa5777069ca4c0658eb
postmodern <postmodern.mod3@gmail.com>
2010-01-13 21:13:02 -0800

    Added basic specs for the BelongsToHandler.

---
+++ spec/belongs_to_handler_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+require 'helpers/examples'
+
+require 'yard-dm/belongs_to_handler'
+require 'yard-dm/legacy/belongs_to_handler'
+
+describe "BelongsToHandler" do
+  include Helpers::Examples
+
+  before(:all) do
+    parse_file :simple_belongs_to
+  end
+
+  it "should define reader methods for the belongs_to relationships" do
+    yard('SimpleBelongsTo#author').should be_instance_of(CodeObjects::MethodObject)
+  end
+
+  it "should define writer methods for the belongs_to relationships" do
+    yard('SimpleBelongsTo#author=').should be_instance_of(CodeObjects::MethodObject)
+  end
+end
