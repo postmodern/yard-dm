5c66897a40ed5bd2dbd16fbc7d266b81ade30c25
postmodern <postmodern.mod3@gmail.com>
2010-01-13 22:27:45 -0800

    Refactored the HashHandler for Ruby 1.9.

--- lib/yard-dm/has_handler.rb
+++ lib/yard-dm/has_handler.rb
@@ -12,35 +12,44 @@ module YARD
 
       def process
         args = statement.parameters
-        n = args[0].last
+        n = args[0]
         name = args[1].last
 
-        if (name.type == :symbol &&
-            ((n.type == :int && n.source =~ /^[0-9]\d*$/) ||
-             (n.type == :ident && n.source == 'n')))
-          nobj = effected_namespace
-          mscope = scope
-          name = name.source[1..-1]
+        return unless name.type == :symbol
 
-          register MethodObject.new(nobj, name, :class) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def self.#{name}"
-            o.parameters = [['repository', 'nil']]
-          end
+        return unless (
+          (n.type == :int && n.source =~ /^[1-9]\d*$/) ||
+          (n.type == :var_ref && n.source == 'n') ||
+          (n.type == :dot2 && (
+            (n[0].type == :int && n[0].source =~ /^[0-9]\d*$/) || (
+              (n[1].type == :int && n[1].source =~ /^[1-9]\d*$/) ||
+              (n[1].type == :var_ref && n[1].source == 'n')
+            )
+          ))
+        )
 
-          register MethodObject.new(nobj, name, mscope) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def #{name}"
-          end
+        nobj = effected_namespace
+        mscope = scope
+        name = name.source[1..-1]
 
-          register MethodObject.new(nobj, "#{name}=", mscope) do |o|
-            o.visibility = :public
-            o.source = statement.source
-            o.signature = "def #{name}="
-            o.parameters = [["new_#{name}", nil]]
-          end
+        register MethodObject.new(nobj, name, :class) do |o|
+          o.visibility = :public
+          o.source = statement.source
+          o.signature = "def self.#{name}"
+          o.parameters = [['repository', 'nil']]
+        end
+
+        register MethodObject.new(nobj, name, mscope) do |o|
+          o.visibility = :public
+          o.source = statement.source
+          o.signature = "def #{name}"
+        end
+
+        register MethodObject.new(nobj, "#{name}=", mscope) do |o|
+          o.visibility = :public
+          o.source = statement.source
+          o.signature = "def #{name}="
+          o.parameters = [["new_#{name}", nil]]
         end
       end
 
