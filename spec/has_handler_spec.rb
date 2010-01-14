783842ef1b13ae0552859cec36b613baa1497a69
postmodern <postmodern.mod3@gmail.com>
2010-01-13 21:51:10 -0800

    Added spes for "has 0..n" and "has 1..n" relationships.

--- spec/has_handler_spec.rb
+++ spec/has_handler_spec.rb
@@ -60,4 +60,40 @@ describe "HasHandler" do
       yard('HasOne#things=').should be_instance_of(CodeObjects::MethodObject)
     end
   end
+
+  describe "has 0..n" do
+    before(:all) do
+      parse_file :has_zero_to_n
+    end
+
+    it "should define class-methods for the 'has 0..n' relationships" do
+      yard('HasZeroToN.things').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define reader methods for the 'has 0..n' relationships" do
+      yard('HasZeroToN#things').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define writer methods for the 'has 0..n' relationships" do
+      yard('HasZeroToN#things=').should be_instance_of(CodeObjects::MethodObject)
+    end
+  end
+
+  describe "has 1..n" do
+    before(:all) do
+      parse_file :has_one_to_n
+    end
+
+    it "should define class-methods for the 'has 1..n' relationships" do
+      yard('HasOneToN.things').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define reader methods for the 'has 1..n' relationships" do
+      yard('HasOneToN#things').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define writer methods for the 'has 1..n' relationships" do
+      yard('HasOneToN#things=').should be_instance_of(CodeObjects::MethodObject)
+    end
+  end
 end
