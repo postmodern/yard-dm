307d80d44b2fc32d23be9371389a6be5ea441304
postmodern <postmodern.mod3@gmail.com>
2010-01-13 21:18:28 -0800

    Added basic specs for the HasHandler.

---
+++ spec/has_handler_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+require 'helpers/examples'
+
+require 'yard-dm/has_handler'
+require 'yard-dm/legacy/has_handler'
+
+describe "HasHandler" do
+  include Helpers::Examples
+
+  describe "has n" do
+    before(:all) do
+      parse_file :simple_has_n
+    end
+
+    it "should define reader methods for the 'has n' relationships" do
+      yard('SimpleHasN#things').should be_instance_of(CodeObjects::MethodObject)
+    end
+
+    it "should define writer methods for the 'has n' relationships" do
+      yard('SimpleHasN#things=').should be_instance_of(CodeObjects::MethodObject)
+    end
+  end
+end
